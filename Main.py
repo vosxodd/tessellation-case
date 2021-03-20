ff6bbc64696b4f8e1f3e04c0f5f2820f652d922f
ikrut555 <44076851+ikrut555@users.noreply.github.com>
2021-03-20 17:30:00 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -22,6 +22,9 @@ def get_num_hexagons():
             n=input("Оно должно быть от 4 до 20. Пожалуйста, повторите попытку: ")
 
 def turtle_area():
+    t.up()
+    t.goto(-100,-100)
+    t.down()
     t.pensize(3)
     t.left(90)
     t.fd(500)
@@ -38,19 +41,75 @@ def right_fd(angle,side_len, times):
         t.fd(side_len)
         times -= 1
 
-def draw_hexagon(x, y, side_len, color):
+def draw_hexagon(x, y, count, color,):
+    for j in range(0,count):
+                t.up()
+                t.goto(x,y)
+                t.down()
+                x_side = (72.17*4/count * sqrt(3)) / 2
+                y_side = 72.17*4/count / 2
+                t.setheading(-90)
+                t.up()
+                t.fd(y_side)
+                t.down()
+                t.pencolor("black")
+                t.fillcolor(color)
+                t.begin_fill()
+                t.left(120)
+                t.fd(72.17*4/count)
+                right_fd(60, 72.17*4/count, 6)
+                t.end_fill()
+                x=x+(125*(4/count))
+    x=-100
+    y=y-(125*(4/count))
+    t.up()
     t.goto(x,y)
-    x_side = (side_len * sqrt(3)) / 2
-    y_side = side_len / 2
-    t.setheading(-90)
-    t.fd(y_side)
-    t.pencolor(color)
-    t.fillcolor(color)
-    t.begin_fill()
-    t.left(120)
-    t.fd(side_len)
-    right_fd(60, side_len, 6)
-    t.end_fill()
+    t.down()
+    
+    for i in range(2,count+1):
+        if i%2==1:
+            for j in range(0,count):
+                t.up()
+                t.goto(x,y+10*4/count)
+                t.down()
+                x_side = (72.17*4/count * sqrt(3)) / 2
+                y_side = 72.17*4/count / 2
+                t.setheading(-90)
+                t.up()
+                t.fd(y_side)
+                t.down()
+                t.pencolor("black")
+                t.fillcolor(color)
+                t.begin_fill()
+                t.left(120)
+                t.fd(72.17*4/count)
+                right_fd(60, 72.17*4/count, 6)
+                t.end_fill()
+                x=x+(125*(4/count))
+        else:
+            for j in range(0,count):
+                t.up()
+                t.goto(x+62*4/count,y+10*4/count)
+                t.down()
+                x_side = (72.17*4/count * sqrt(3)) / 2
+                y_side = 72.17*4/count / 2
+                t.setheading(-90)
+                t.up()
+                t.fd(y_side)
+                t.down()
+                t.pencolor("black")
+                t.fillcolor("green")
+                t.begin_fill()
+                t.left(120)
+                t.fd(72.17*4/count)
+                right_fd(60, 72.17*4/count, 6)
+                t.end_fill()
+                x=x+(125*(4/count))
+        x=-100
+        y=y-(115*(4/count))
+        t.up()
+        t.goto(x,y)
+        t.down()
 
 
 
@@ -58,6 +117,7 @@ def draw_hexagon(x, y, side_len, color):
 
 turtle_area()
 count=get_num_hexagons()
-draw_hexagon(0,400,50,'red')
+draw_hexagon(-100,397,count,'red')
 
 t.mainloop()
+
